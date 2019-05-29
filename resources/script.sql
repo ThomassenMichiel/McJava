263d29bda893a77d432adc6d1a13dd965816931e
Michiel Thomassen <bunzzbunzz@gmail.com>
2019-05-30 00:51:11 +0200

    Create the base database script.

---
+++ resources/script.sql
@@ -0,0 +1,58 @@
+CREATE TABLE IF NOT EXISTS ingredient(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  current_stock INT
+);
+
+CREATE TABLE IF NOT EXISTS product(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  ingredients INT,
+  price DECIMAL(6,2)
+);
+
+CREATE TABLE IF NOT EXISTS menu(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  products INT
+);
+
+CREATE TABLE IF NOT EXISTS product_ingredient(
+  ingredient_id INT,
+  product_id INT,
+  amount INT,
+  CONSTRAINT `fk_ingredient_id` FOREIGN KEY (ingredient_id) REFERENCES ingredient(id),
+  CONSTRAINT `fk_product_ingredient` FOREIGN KEY (product_id) REFERENCES product(id)
+);
+
+CREATE TABLE IF NOT EXISTS menu_product(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  product_id INT,
+  menu_id INT,
+  CONSTRAINT `fk_menu_id` FOREIGN KEY (menu_id) REFERENCES menu(id),
+  CONSTRAINT `fk_product_id` FOREIGN KEY (product_id) REFERENCES product(id)
+);
+
+CREATE TABLE IF NOT EXISTS order_item(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  product_id INT,
+  amount INT,
+  total_price DECIMAL(6,2),
+  finished BOOL,
+  FOREIGN KEY (product_id) REFERENCES product(id)
+);
+
+CREATE TABLE IF NOT EXISTS customer_order(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(30),
+  telephone_number VARCHAR(15),
+  items_to_order INT,
+  finished_cooking BOOL,
+  ordered BOOL
+);
+
+CREATE TABLE IF NOT EXISTS customer_order_items(
+  customer_order_id INT,
+  order_item_id INT,
+  CONSTRAINT `fk_customer_order_items_to_order_item` FOREIGN KEY (order_item_id) REFERENCES order_item(id),
+  CONSTRAINT `fk_customer_order_items_to_customer_order` FOREIGN KEY (customer_order_id) REFERENCES customer_order(id)
+);
