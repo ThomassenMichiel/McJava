6342424581ea898b518f812ab31580dbd2fafbd9
Fonthom <thomasf0n7a1n3@gmail.com>
2019-05-30 15:09:49 +0200

    welcome screen that the customer sees.

--- src/be/mcjava/CustomerMain.java
+++ src/be/mcjava/CustomerMain.java
@@ -13,8 +13,8 @@ public class CustomerMain extends Application {
     }
     @Override
     public void start(Stage stage) throws Exception{
-        Parent root = FXMLLoader.load( getClass().getResource("CustomerScreen.fxml"  ) );
-        Scene scene = new Scene( root, 400,400 );
+        Parent root = FXMLLoader.load( getClass().getResource( "mcJavaCustomerFXML/CustomerScreen.fxml" ) );
+        Scene scene = new Scene( root, 650,450 );
         stage.setTitle( "CustomerScreen" );
         stage.setScene( scene );
         stage.show();
