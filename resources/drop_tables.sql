732b19dab53c67669d2ba37719306333f783bc91
lens <lens.huygh@gmail.com>
2019-05-31 01:34:24 +0200

    added 3 sql scripts: drop_tables.sql: disables FK key check, drop all tables and re-enables FK check insert_ingredients:  inserts all ingredients needed for th next script insert_products: makes some ingredients into products and adds products and a few menu's

---
+++ resources/drop_tables.sql
@@ -0,0 +1,10 @@
+SET FOREIGN_KEY_CHECKS = 0;
+drop table  ingredient;
+drop table  product;
+drop table  premade_menu;
+drop table  premade_menu_product;
+drop table  product_ingredient;
+drop table  order_item;
+drop table  customer_order;
+drop table  customer_order_items;
+SET FOREIGN_KEY_CHECKS = 1;
