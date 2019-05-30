f61fad5a04d6b15acde23b9ec560da19b60e9bb1
Fonthom <thomasf0n7a1n3@gmail.com>
2019-05-30 22:11:17 +0200

    create check methods for name and phone-number.

--- src/be/mcjava/fxml/CustomerScreenController.java
+++ src/be/mcjava/fxml/CustomerScreenController.java
@@ -8,6 +8,16 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class CustomerScreenController {
+
+    //checks if it is a string with only letters.
+    public boolean isName(String name){
+      return name.chars().allMatch(Character::isLetter);
+
+    }
+    //checks valid phone-number
+    public boolean isPhoneNumber(String num){
+        return true;
+    }
     @FXML
     private void continuePressed(ActionEvent event) throws Exception{
      FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("CustomerScreen2.fxml"));
