de62352760a9b4c8a4a114a1cc03840f483c4a37
lens <lens.huygh@gmail.com>
2019-05-31 15:40:46 +0200

    combined 2 insert-scripts into 'insert_data.sql' removed 2 original insert scripts

--- resources/insert_data.sql
+++ resources/insert_data.sql
@@ -1,3 +1,89 @@
+#   first create all ingredients referenced by prodcuts are created
+
+insert into ingredient values (1,'chicken_for_wrap',400);
+insert into ingredient values (2,'wrap_for_wrap',100);
+insert into ingredient values (3,'assorted_vegetables_for_wrap',100);
+insert into ingredient values (4,'wrap_for_wrap_small',100);
+insert into ingredient values (5,'bun_mcfish',100);
+insert into ingredient values (6,'fish_patty',100);
+insert into ingredient values (7,'lettuce',500);
+insert into ingredient values (8,'burger_sauce_mcfish',100);
+insert into ingredient values (9,'bun_hamburger',100);
+insert into ingredient values (10,'hamburger_patty',100);
+insert into ingredient values (11,'pickles',100);
+insert into ingredient values (12,'burger_sauce_hamburger',400);
+insert into ingredient values (13,'cheeseburger_cheese',400);
+insert into ingredient values (14,'checken_mcnugget',600);
+insert into ingredient values (15,'veggy_mcnugget',10);
+insert into ingredient values (16,'fries_small',500);
+insert into ingredient values (17,'carrots_small',100);
+insert into ingredient values (18,'drink_cola_25',1000);
+insert into ingredient values (19,'drink_cola_zero_25',1000);
+insert into ingredient values (20,'drink_cola_light_25',1000);
+insert into ingredient values (21,'drink_sprite_25',1000);
+insert into ingredient values (22,'drink_fanta_25',1000);
+insert into ingredient values (23,'drink_applejuice_25',1000);
+insert into ingredient values (24,'drink_orangejuice_25',1000);
+insert into ingredient values (25,'drink_water_flat_25',1000);
+insert into ingredient values (26,'drink_water_sparkling_25',1000);
+insert into ingredient values (27,'drink_fristi_25',1000);
+insert into ingredient values (28,'petit_lu',50);
+insert into ingredient values (29,'icecream_cone',50);
+insert into ingredient values (30,'ice_squeeze',50);
+insert into ingredient values (31,'pineapple_chunks',50);
+insert into ingredient values (32,'actimel',50);
+insert into ingredient values (33,'danonino',50);
+insert into ingredient values (34,'fries_medium',500);
+insert into ingredient values (35,'salad_mixed_medium',100);
+insert into ingredient values (36,'drink_water_flat_33',100);
+insert into ingredient values (37,'drink_water_sparkling_33',100);
+insert into ingredient values (38,'drink_orangejuice_33',100);
+insert into ingredient values (39,'drink_cola_33',1000);
+insert into ingredient values (40,'drink_cola_zero_33',1000);
+insert into ingredient values (41,'drink_cola_light_33',1000);
+insert into ingredient values (42,'drink_sprite_33',1000);
+insert into ingredient values (43,'drink_fanta_33',1000);
+insert into ingredient values (44,'fries_large',1000);
+insert into ingredient values (45,'salad_mixed_large',1000);
+insert into ingredient values (46,'drink_water_flat_50',100);
+insert into ingredient values (47,'drink_water_sparkling_50',100);
+insert into ingredient values (48,'drink_orangejuice_50',100);
+insert into ingredient values (49,'drink_cola_50',1000);
+insert into ingredient values (50,'drink_cola_zero_50',1000);
+insert into ingredient values (51,'drink_cola_light_50',1000);
+insert into ingredient values (52,'drink_sprite_50',1000);
+insert into ingredient values (53,'drink_fanta_50',1000);
+insert into ingredient values (54,'salad_cheese_bacon',150);
+insert into ingredient values (55,'salad_pesto_mozzarella',150);
+insert into ingredient values (56,'salad_ceasar',150);
+insert into ingredient values (57,'bun_don_j',200);
+insert into ingredient values (58,'patty_beef_100',1000);
+insert into ingredient values (59,'cheeze_chrizo_mancheo',200);
+insert into ingredient values (60,'burger_sauce_don_j',200);
+insert into ingredient values (61,'tapenade_don_j',200);
+insert into ingredient values (62,'tomatoes_sun_dried',200);
+insert into ingredient values (63,'burger_salad_batavia',200);
+insert into ingredient values (64,'burger_bacon_extra_long',200);
+insert into ingredient values (65,'cheese_maredsous',200);
+insert into ingredient values (66,'tomato_slice',500);
+insert into ingredient values (67,'red_onion_slice',500);
+insert into ingredient values (68,'burger_sauce_lemon_mayo',200);
+insert into ingredient values (69,'bun_brioche_lineseed',200);
+insert into ingredient values (70,'patty_chicken_filet',200);
+insert into ingredient values (71,'burger_salad_standard',500);
+insert into ingredient values (72,'bun_big_mac',500);
+insert into ingredient values (73,'cheese_cheddar',500);
+insert into ingredient values (74,'standard_onion_slice',500);
+insert into ingredient values (75,'burger_sauce_big_mac',500);
+insert into ingredient values (76,'burger_sauce_ketchup',500);
+insert into ingredient values (77,'burger_sauce_mustard',500);
+
+#   create products and use product_ingredient to attach one or more ingredients to a product
+#   4 premade menus are created
+#   only 1 product is attached to a premade_menu by using premade_menu_product (carrots is attached to the Happy Meal)
+#   because thats the only thing that is default in a meal
+#   all the rest in all premade_menu s is to be chosen when ordering
+
     insert into product values (1,'McWrap Chicken',4);
         insert into product_ingredient values (1,1,3);
         insert into product_ingredient values (1,2,1);
