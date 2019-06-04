d515d733a698463caa0c47f01f871d26829f5074
Fonthom <thomasf0n7a1n3@gmail.com>
2019-06-04 16:44:27 +0200

    create methods with pseudo code in comments

--- src/be/mcjava/controller/KitchenController.java
+++ src/be/mcjava/controller/KitchenController.java
@@ -1,9 +1,42 @@
 package be.mcjava.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.layout.VBox;
+
+import java.io.FileNotFoundException;
+import java.sql.SQLException;
 
 public class KitchenController {
     @FXML
     private VBox contentvbox;
+
+    @FXML
+    private void initialize() throws FileNotFoundException {
+        try {
+            getOrderData();
+            addOrderToVBox();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    private void getOrderData(){
+        //gets orders from DB
+        //refreshes every 30 secs.
+    }
+    private void addOrderToVBox(){
+        // creates a text version of the order in the view.
+        // with every order comes a "finished" button.
+        // the finished button has onAction = "#finishedIsPressed".
+    }
+    @FXML
+    private void finishedIsPressed (ActionEvent event){
+        // add order to list finished orders.
+        // when pressed initialize remove order.
+        // when pressed
+    }
+    private void removeOrder(){
+        // removes order initialized trough an action event method.
+    }
+
 }
