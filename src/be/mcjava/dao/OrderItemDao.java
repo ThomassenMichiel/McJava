44333c797a2e17e3621294550122232187641162
Lens Huygh <lens.huygh@gmail.com>
2019-06-05 22:14:39 +0200

    - added OrderItemDao to get the orderItems into the DB

--- src/be/mcjava/dao/OrderItemDao.java
+++ src/be/mcjava/dao/OrderItemDao.java
@@ -2,10 +2,23 @@ package be.mcjava.dao;
 
 import be.mcjava.model.AbstractOrderItem;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 public class OrderItemDao {
     public static void saveOrderItems(List<AbstractOrderItem> itemsToOrder) {
-
+        String sql = "insert into order_item (product_id,amount,total_price,finished) values ?,?,?,?";
+        try(PreparedStatement preparedStatement = DaoConnector.getConnection().prepareStatement(sql)) {
+            for (AbstractOrderItem abstractOrderItem : itemsToOrder) {
+                preparedStatement.setLong(1,abstractOrderItem.getId());
+                preparedStatement.setInt(2,abstractOrderItem.getAmount());
+                preparedStatement.setBigDecimal(3,abstractOrderItem.getPrice());
+                preparedStatement.setBoolean(4,false);
+                preparedStatement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
