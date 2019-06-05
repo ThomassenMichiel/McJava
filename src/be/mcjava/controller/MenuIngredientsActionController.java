4457fa5c5cdc84e2e0f68e33c7e1b7c8fc9a31c4
lens <lens.huygh@gmail.com>
2019-06-05 17:17:56 +0200

    - MenuIngredients changed layout; each product category has a column now to choose from, chosen products are moved to a lower row when clicked, end moved a row up again when clicked again

--- src/be/mcjava/controller/MenuIngredientsActionController.java
+++ src/be/mcjava/controller/MenuIngredientsActionController.java
@@ -1,11 +1,11 @@
 package be.mcjava.controller;
 
 import be.mcjava.dao.AllowedMenuProductsDao;
-import be.mcjava.dao.ProductsDao;
 import be.mcjava.model.AllowedMenuProduct;
 import be.mcjava.model.Product;
 import be.mcjava.service.ChosenProductService;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
@@ -28,38 +28,64 @@ public class MenuIngredientsActionController {
     private List<Product> productList;
 
     @FXML
-    public void initialize(){
+    public void initialize() {
         AllowedMenuProductsDao allowedMenuProductsDao = new AllowedMenuProductsDao();
         List<AllowedMenuProduct> allowedMenuProductList = allowedMenuProductsDao.getAllowedMenuProductsByPremadeMenuName(ChosenProductService.preMadeMenu.getName());
-        productsOverviewVBox = new VBox();
+        //productsOverviewVBox = new VBox();
+        //get number of columns needed
+        int columnsNeeded = 0;
+        for (AllowedMenuProduct allowedMenuProduct : allowedMenuProductList) {
+            if (allowedMenuProduct.getItemPositionInMenu() > columnsNeeded) {
+                columnsNeeded = allowedMenuProduct.getItemPositionInMenu();
+            }
+        }
+        //create vboxes inside all needed columns on the gridPane and one row lower for chosen products
+        for (int i = 0; i < columnsNeeded; i++) {
+            VBox vBox = new VBox();
+            mainproductsgrid.add(vBox, i, 0);
+            VBox vBox2 = new VBox();
+            mainproductsgrid.add(vBox2, i, 1);
+        }
         for (AllowedMenuProduct allowedMenuProduct : allowedMenuProductList) {
             Button button = new Button(allowedMenuProduct.getProductName());
             button.setOnMouseClicked(mouseEvent -> menuProductClicked(mouseEvent));
-            productsOverviewVBox.getChildren().add(button);
+            VBox vBox = (VBox) getNodeFromGridPane(mainproductsgrid, allowedMenuProduct.getItemPositionInMenu() - 1, 0);
+            vBox.getChildren().add(button);
         }
-        mainproductsgrid.add(productsOverviewVBox,0,0);
-        chosenVBox = new VBox();
-        mainproductsgrid.add(chosenVBox,1,0);
+    }
+
+    private Node getNodeFromGridPane(GridPane gridPane, int col, int row) {
+        for (Node node : gridPane.getChildren()) {
+            if (GridPane.getColumnIndex(node) == col && GridPane.getRowIndex(node) == row) {
+                return node;
+            }
+        }
+        return null;
     }
 
     @FXML
     private void menuProductClicked(MouseEvent mouseEvent) {
         Button clickedButton = (Button) mouseEvent.getSource();
-        String chosenProductName = clickedButton.getText();
-        removeProductFromOverview(clickedButton);
-        chosenVBox.getChildren().add(clickedButton);
+        VBox clickedButtonParentVBox = (VBox) clickedButton.getParent();
+        Integer clickedGridPaneColumnIndex = GridPane.getColumnIndex(clickedButtonParentVBox);
+        removeProductFromOverview(clickedButton, clickedButtonParentVBox, clickedGridPaneColumnIndex);
     }
 
-    private void removeProductFromOverview(Button buttonToRemove) {
-        buttonToRemove.setOnMouseClicked(mouseEvent -> selectMenuProductClicked(mouseEvent));
-        productsOverviewVBox.getChildren().remove(buttonToRemove);
+    private void removeProductFromOverview(Button buttonToRemove, VBox fromVBox, Integer clickedGridPaneColumnIndex) {
+        buttonToRemove.setOnMouseClicked(mouseEvent -> chosenMenuProductClicked(mouseEvent));
+        VBox targetVBox = (VBox) getNodeFromGridPane(mainproductsgrid, clickedGridPaneColumnIndex, 1);
+        targetVBox.getChildren().add(buttonToRemove);
+        fromVBox.getChildren().remove(buttonToRemove);
     }
 
     @FXML
-    private void selectMenuProductClicked(MouseEvent mouseEvent) {
+    private void chosenMenuProductClicked(MouseEvent mouseEvent) {
         Button clickedButton = (Button) mouseEvent.getSource();
         clickedButton.setOnMouseClicked(mouseEvent2 -> menuProductClicked(mouseEvent2));
-        productsOverviewVBox.getChildren().add(clickedButton);
-        chosenVBox.getChildren().remove(clickedButton);
+        VBox clickedVBox = (VBox) clickedButton.getParent();
+        clickedVBox.getChildren().remove(clickedButton);
+        Integer clickedGridPaneColumnIndex = GridPane.getColumnIndex(clickedVBox);
+        VBox targetVBox = (VBox) getNodeFromGridPane(mainproductsgrid, clickedGridPaneColumnIndex, 0);
+        targetVBox.getChildren().add(clickedButton);
     }
 }
