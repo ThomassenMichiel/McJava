9920896ffc19d9e10c0d64968eb78ae3d30a8758
Fonthom <thomasf0n7a1n3@gmail.com>
2019-06-09 08:16:15 +0200

    pasted the right stuff

--- src/be/mcjava/dao/CookingOrderDao.java
+++ src/be/mcjava/dao/CookingOrderDao.java
@@ -1,5 +1,29 @@
 package be.mcjava.dao;
 
-public class CookingOrderDao {
 
+import be.mcjava.model.CookingOrders;
+import be.mcjava.model.CustomerOrder;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CookingOrderDao {
+    private Long id;
+    private List<CustomerOrder> ordersToCook = new ArrayList<>();
+    private boolean finished;
+
+
+/**    public ArrayList<CustomerOrder> getOrdersToCook()throws SQLException {
+ String sql = "Select * From Customer";
+ PreparedStatement preparedStatement = DaoConnector.getConnection().prepareStatement( sql );
+ ResultSet resultSet = preparedStatement.executeQuery();
+
+ while (resultSet.next()) {
+ CookingOrders cookingOrder = new CookingOrders(resultSet.getLong("id"), resultSet.getClob ("orders to cook"));
+ ordersToCook.add(cookingOrder);
+
+ return ordersToCook;
+ }return null;
+ }*/
 }
