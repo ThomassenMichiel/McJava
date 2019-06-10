2882a1b902f0637c0073ca8e0da856d46240792e
Michiel Thomassen <bunzzbunzz@gmail.com>
2019-06-10 22:19:21 +0200

    Receipt now shows price per individual items Added constraints and alignment for the new row Cleaned the initialize method

--- src/be/mcjava/controller/CustomerReceiptController.java
+++ src/be/mcjava/controller/CustomerReceiptController.java
@@ -10,11 +10,15 @@ import javafx.fxml.FXML;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.control.Separator;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 
 public class CustomerReceiptController {
@@ -31,32 +35,58 @@ public class CustomerReceiptController {
     public void initialize() {
         CustomerOrder customerOrder = CustomerOrderService.customerOrder;
         setConstraints();
-    
-        id.setText("== " + customerOrder.getId() + " ==");
-        id.setFont(Font.font(24));
-        id.setTextAlignment(TextAlignment.CENTER);
-        GridPane.setHalignment(id,HPos.CENTER);
-        GridPane.setMargin(id,new Insets(30,0,30,0));
+        
+        displayIdText(customerOrder);
+        
+        createColumnHeaders();
+        
         customerOrder.getItemsToOrder().forEach(this::addItemsToDisplay);
         setTotalPrice(customerOrder);
         
-        GridPane.setRowIndex(closingButton,row);
+        GridPane.setRowIndex(closingButton, row);
+    }
+    
+    private void createColumnHeaders() {
+        Text productText = new Text("Product");
+        Text amountText = new Text("Amount");
+        Text ppuText = new Text("Unit");
+        Text totalText = new Text("Total Unit");
+        
+        items.add(productText, column++, row);
+        items.add(amountText, column++, row);
+        items.add(ppuText, column++, row);
+        items.add(totalText, column++, row);
+        
+        row++;
+        
+        
+        column = 0;
+    }
+    
+    private void displayIdText(CustomerOrder customerOrder) {
+        id.setText("== " + customerOrder.getId() + " ==");
+        id.setFont(Font.font(24));
+        id.setTextAlignment(TextAlignment.CENTER);
+        GridPane.setHalignment(id, HPos.CENTER);
+        GridPane.setMargin(id, new Insets(30, 0, 30, 0));
     }
     
     private void setTotalPrice(CustomerOrder customerOrder) {
         String formattedPrice = String.format("%5.2f", customerOrder.getFinalPrice());
-        Text totalText = new Text("Total:");
+        Text totalText = new Text("Grand total:");
         Text formattedPriceText = new Text(formattedPrice);
-        GridPane.setMargin(totalText,new Insets(30,0,0,0));
-        GridPane.setMargin(formattedPriceText,new Insets(30,0,0,0));
-        items.addRow(row++,new Text(),totalText,formattedPriceText);
+        GridPane.setMargin(totalText, new Insets(30, 0, 0, 0));
+        GridPane.setMargin(formattedPriceText, new Insets(30, 0, 0, 0));
+        items.addRow(row++, new Text(), totalText, formattedPriceText);
     }
     
     private void setConstraints() {
         items.getColumnConstraints().add(new ColumnConstraints(200));
         items.getColumnConstraints().add(new ColumnConstraints(50));
-        items.getColumnConstraints().add(new ColumnConstraints(50));
+        items.getColumnConstraints().add(new ColumnConstraints(75));
+        items.getColumnConstraints().add(new ColumnConstraints(75));
         items.getColumnConstraints().get(2).setHalignment(HPos.RIGHT);
+        items.getColumnConstraints().get(3).setHalignment(HPos.RIGHT);
     }
     
     private void addItemsToDisplay(AbstractOrderItem item) {
@@ -69,19 +99,23 @@ public class CustomerReceiptController {
     }
     
     private void createText(SingleOrderItem item) {
-        items.add(new Text(item.getItems().getName()),column++,row);
-        items.add(new Text(String.valueOf(item.getAmount())),column++,row);
+        items.add(new Text(item.getItems().getName()), column++, row);
+        items.add(new Text(String.valueOf(item.getAmount())), column++, row);
+        String individualPrice = String.format("%5.2f", item.getItems().getPrice());
+        items.add(new Text(individualPrice), column++, row);
         String price = String.format("%5.2f", item.getPrice());
-        items.add(new Text(price),column++,row);
+        items.add(new Text(price), column++, row);
         row++;
         column = 0;
     }
     
     private void createText(PreMadeOrderMenu item) {
-        items.add(new Text(item.getName()),column++,row);
-        items.add(new Text(String.valueOf(item.getAmount())),column++,row);
+        items.add(new Text(item.getName()), column++, row);
+        items.add(new Text(String.valueOf(item.getAmount())), column++, row);
+        String individualPrice = String.format("%5.2f", item.getPrice().divide(new BigDecimal(item.getAmount()), RoundingMode.HALF_EVEN));
+        items.add(new Text(individualPrice), column++, row);
         String price = String.format("%5.2f", item.getPrice());
-        items.add(new Text(price),column++,row);
+        items.add(new Text(price), column++, row);
         row++;
         item.getItems().forEach(this::addEachItemInAMenu);
         column = 0;
@@ -89,7 +123,7 @@ public class CustomerReceiptController {
     
     private void addEachItemInAMenu(SingleOrderItem item) {
         String itemEntry = "\t- " + item.getItems().getName();
-        items.add(new Text(itemEntry),0,row++);
+        items.add(new Text(itemEntry), 0, row++);
     }
     
     public void returnToMainMenu(ActionEvent actionEvent) {
