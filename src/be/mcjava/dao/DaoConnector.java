c49245eb17fbace5047e3befd9a6af5b42a2aef3
Michiel <mich.thomassen@gmail.com>
2019-06-11 15:48:07 +0200

    Database credentials are relegated to application.properties
    Program loads application.properties before running. It will throw an
    exception if invalid and quit when the alert is closed.

--- src/be/mcjava/dao/DaoConnector.java
+++ src/be/mcjava/dao/DaoConnector.java
@@ -5,16 +5,32 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DaoConnector {
-//    private static final String URL = "jdbc:mysql://192.168.99.100/mcjava";
-    private static final String URL = "jdbc:mysql://localhost/mcjava";
-    private static final String USER = "root";
-//    private static final String PASSWORD = "password";
-    //private static final String PASSWORD = "mypass";
-    private static final String PASSWORD = "root";
-
+//    private String URL = "jdbc:mysql://192.168.99.100/mcjava";
+//    private static String URL = "jdbc:mysql://localhost/mcjava";
+//    private static String USER = "root";
+//    private String PASSWORD = "password";
+//    private String PASSWORD = "mypass";
+//    private static String PASSWORD = "root";
+    
+    private static String URL;
+    private static String USER;
+    private static String PASSWORD;
+    
     private DaoConnector() {}
     
     public static Connection getConnection() throws SQLException{
             return DriverManager.getConnection(URL,USER,PASSWORD);
+    }
+    
+    public static void setURL(String URL) {
+        DaoConnector.URL = URL;
+    }
+    
+    public static void setUSER(String USER) {
+        DaoConnector.USER = USER;
+    }
+    
+    public static void setPASSWORD(String PASSWORD) {
+        DaoConnector.PASSWORD = PASSWORD;
     }
 }
